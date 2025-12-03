ccb7c5fae0064de37ce3050363001b98980b1522
nmaserang <nmaserang@berkeley.edu>
2025-12-03 15:08:32 -0500

    refactored pipeline and added deps to Project.toml

--- src/FibTN.jl
+++ src/FibTN.jl
@@ -2,9 +2,22 @@ module FibTN
 
 end # module FibTN
 
-using .FibTN
-
 include("tensorbuilder.jl")
 include("networkbuilder.jl")
 include("networkvisualizer.jl")
 include("latticebuilder.jl")
+
+case = length(ARGS) > 0 ? ARGS[1] : throw(ErrorException("case number must be supplied as the first argument"))
+genTN = length(ARGS) > 1 && ARGS[2] == "1" ? true : false
+contractTN = length(ARGS) > 2 && ARGS[3] == "1" ? true : false
+
+@show case, genTN, contractTN
+
+include("GS$(case).jl")
+@show rsg
+if genTN
+    include("genTN.jl")
+    if contractTN
+        include("contractTN.jl")
+    end
+end
--- src/contractTN.jl
+++ src/contractTN.jl
@@ -1,26 +1,8 @@
 # before this script is run, rsg, contractionsequences, pindict, offset, scale, and
 # nlabeloffsetscale should've been added to the environment
-
-# cap the rsg
-cap_all!(rsg)
-
-# generate the other graphs
-ig = rsg2ig(rsg)
-qg = ig2qg(ig)
-tg = ig2tg(ig)
-
-# generate the networklayout to display the qg
-for (k, v) in pindict
-    pindict[k] = scale .* v .+ offset
-end
-l = NetworkLayout.Spring(pin=pindict)
-
-# display the tg before contraction
-f = Figure()
-_, _, axs = getaxisgrid(f, 1)
-p = tgplot(axs[1], tg, layout=l)
-finalize(f, axs)
-display(f)
+#
+# before this script is run,  genTN.jl should be run to add ig, tg, qg, and the NetworkLayout
+# to the env
 
 # contract the tg
 contractcaps!(tg)
@@ -36,3 +18,5 @@ w, h, axs = getaxisgrid(f, length(s))
 plots = statesplot(axs, qg, s, vlabels=false, layout=l, popoutargs=Dict(:titlesize=>28))
 finalize(f, axs)
 display(f)
+
+# store results?
