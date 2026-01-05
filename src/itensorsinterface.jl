a3793443767545f75451306cc097312186f56d8a
nmaserang <nmaserang@berkeley.edu>
2026-01-04 17:03:54 -0800

    passed all tests

--- src/itensorsinterface.jl
+++ src/itensorsinterface.jl
@@ -3,32 +3,47 @@ module ITensorsInterface
 using ..TensorHandles
 using ITensors
 
+export ITensorsBackend, TensorHandle, contract, trace
+
 struct ITensorsBackend <: AbstractBackend end
 
 function TensorHandle(::Type{ITensorsBackend}, tensor_data::AbstractArray, ids::Vector{IndexData})
     itensorindices = [Index(id.dim) for id in ids]
     index_map = Dict(id => iti for (id, iti) in zip(ids, itensorindices))
     tensor = ITensor(tensor_data, itensorindices...)
-    TensorHandle{ITensorsBackend}(tensor, index_map)
+    TensorHandle{ITensorsBackend, ITensor, Index}(tensor, index_map)
 end
-    
+
 function contract(th1::TensorHandle{ITensorsBackend}, th2::TensorHandle{ITensorsBackend}, cs::ContractionSpec)
-    contractedindices = []
+    # check that there are no duplicate indices between the two tensors
+    d = merge(th1.index_map, th2.index_map)
+    if length(d) != length(th1.index_map) + length(th2.index_map)
+        error("tensor handle indices were not unique")
+    end
+    # make new tensor
     new_tensor = th1.tensor
-    for p in cs 
-        new_tensor = new_tensor * δ(th1.index_map[p.first], th2.index_map[p.second])
-        push!(contractedindices, p.first, p.second)
+    for p in cs.pairs 
+        new_tensor = new_tensor * δ(d[p.first], d[p.second])
     end
     new_tensor = new_tensor * th2.tensor
-    
+    # make new index map
     new_index_map = filter(merge(th1.index_map, th2.index_map)) do kv
-        kv.first ∉ contractedindices
+        kv.first ∉ cs.indices
     end
-    TensorHandle{ITensorsBackend}(new_tensor, new_index_map)
+    TensorHandle{ITensorsBackend, ITensor, Index}(new_tensor, new_index_map)
 end
 
 function trace(th::TensorHandle{ITensorsBackend}, cs::ContractionSpec)
-    contract(th, TensorHandle(ITensorsBackend, [], Dict()), cs)
+    # make new tensor
+    new_tensor = th.tensor
+    for p in cs.pairs
+        new_tensor = new_tensor * δ(th.index_map[p.first], th.index_map[p.second])
+    end
+    # make new index map
+    new_index_map = filter(th.index_map) do kv
+        kv.first ∉ cs.indices
+    end
+    TensorHandle{ITensorsBackend, ITensor, Index}(new_tensor, new_index_map)
 end
 
 end # module ITensorsInterface
--- src/tensorhandles.jl
+++ src/tensorhandles.jl
@@ -2,7 +2,7 @@ module TensorHandles
 
 export AbstractBackend
 export VIRT, PHYS, IndexData
-export TensorHandle, ContractionSpec, contract
+export TensorHandle, ContractionSpec, contract, trace
 
 abstract type AbstractBackend end
 
@@ -16,14 +16,26 @@ struct IndexData
 end
 
 struct ContractionSpec
-    pairs::Tuple{Pair{IndexData, IndexData}}
+    pairs::Vector{Pair{IndexData, IndexData}}
+    indices::Set{IndexData}
     ContractionSpec(pairs) = begin
+        indices = Set()
         for p in pairs
+            # check for dimension mismatch
             if p.first.dim != p.second.dim
                 error("dimension mismatch for indices $(p.first) and $(p.second)")
             end
+            # check that indices each appear only once
+            if p.first ∈ indices
+                error("index $(p.first) appears twice")
+            end
+            if p.second ∈ indices
+                error("index $(p.second) appears twice")
+            end
+            push!(indices, p.first)
+            push!(indices, p.second)
         end
-        new(pairs)
+        new(pairs, indices)
     end
 end
 
@@ -34,6 +46,10 @@ end
 
 function contract(::TensorHandle{B}, ::TensorHandle{B}, ::ContractionSpec) where {B <: AbstractBackend}
     error("contract not implemented for backend $(B)")
+end
+
+function trace(::TensorHandle{B}, ::ContractionSpec) where {B <: AbstractBackend}
+    error("trace not implemented for backend $(B)")
 end
 
 end # module TensorHandles
