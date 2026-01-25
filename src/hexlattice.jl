8d2b61b30d9c544b11df5f8a34305abc96ee9d04
nmaserang <nmaserang@berkeley.edu>
2026-01-24 16:21:20 -0800

    switching to just using sparsearraykit and not doing the backend-agnostic nonsense

--- src/hexlattice.jl
+++
@@ -1,8 +0,0 @@
-using ..TensorNetworks
-using ..ITensorsBackend
-using ..TensorHandles
-using ..TensorTypes
-
-plaquette = TensorNetwork()
-
-add_tensor(plaquette, Tensorhandle(ITensorsBackend, tensor_data(Vertex), index_data(Vertex)))
