64e03fc24781852726c01d1f7404b7786b00c1da
nmaserang <nmaserang@berkeley.edu>
2026-01-26 03:50:33 -0800

    first draft of lattice handler

--- src/qubitlattices.jl
+++ src/qubitlattices.jl
@@ -1,20 +1,42 @@
 module QubitLattices
 
-struct QubitLattice
+using Graphs
+using ..Indices
 
-    qubits_from_index::Dict{IndexLabel, NTuple{3, Int}}
-    adjacent_qubits::Dict{Int, Vector{int}}
-    adjacent_tensors::Dict{Int, NTuple{2, Int}}
-    function QubitLattice(ftn::FibTensorNetwork)
-        
-    end
+export QubitLattice, add_index!
+
+struct QubitLattice
+    qubits_from_index::Dict{IndexLabel, Vector{Int}}
+    _node_from_index::Dict{IndexLabel, Int}
+    indices_from_qubit::Dict{Int, Vector{IndexLabel}}
+    _edge_from_qubit::Dict{Int, SimpleEdge}
+    _unpaired_qubits::Vector{Int}
+    graph::SimpleGraph
+    QubitLattice() = new(Dict(), Dict(), Dict(), Dict(), Array(), SimpleGraph())
 end
 
-; qubit_index_map=Dict{IndexLabel, NTuple{3, Int}}()
-    merge!(tn.qubits_from_index, qubit_index_map)
-        # check that all indices in qubit index map are in this tensor
-    for (k, v) in qubit_index_map
-        if k.group != tl.group error("got qubit index map for index not associated with this tensor") end
+function add_index!(ql::QubitLattice, i::IndexLabel, qubits::Vector{Int})
+    # register qubits to index
+    ql.qubits_from_index[i] = qubits
+    # add index to graph
+    add_vertex!(ql.graph)
+    ql._node_from_index[i] = nv(ql.graph)
+    # register indices per qubit, adding edges if an added qubit completes a match
+    for qubit in qubits
+        if haskey(ql.indices_from_qubit, qubit)
+            # there is already another index for this qubit
+            # we are assuming that each qubit can be matched to at most two indices
+            otherindexnode = ql._node_from_index[ql.indices_from_qubit[qubit][1]]
+            add_edge!(ql.graph, nv(ql.graph), otherindexnode)
+            _edge_from_qubit[qubit] = SimpleEdge(nv(ql.graph), otherindexnode)
+            push!(ql.indices_from_qubit[qubit], i)
+            filter!(!=(qubit), ql._unpaired_qubits)
+        else
+            # this is the first index for this qubit
+            ql.indices_from_qubit = [i]
+            push!(ql._unpaired_qubits, qubit)
+        end
     end
+end
 
 end # module QubitLattices
