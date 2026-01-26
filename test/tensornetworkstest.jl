d6aa46c69852359f79913d89a054a48a9898af0b
nmaserang <nmaserang@berkeley.edu>
2026-01-26 10:33:51 -0800

    final updates before dinner

--- test/tensornetworkstest.jl
+++ test/tensornetworkstest.jl
@@ -43,6 +43,9 @@ end
     @test tn._index_use_count[a1] == 1
     @test tn._index_use_count[b1] == 1
     @test tn._index_use_count[c1] == 1
+    
+    # IndexLabels should act by value not by object identity
+    @test tn.tensor_with_index[IndexLabel(1, :a)] == tn.tensor_with_index[a1]
 
     # duplicate IndexLabel should error
     @test_throws ErrorException add_tensor!(tn, TensorLabel(1, [a1]))
@@ -104,4 +107,34 @@ end
     @test length(tn.contractions) == 3
     @test tn._index_use_count[a1] == 2
     @test tn._index_use_count[center1] == 1
+end
+
+@testset "TensorNetwork find" begin
+    tn = TensorNetwork()
+    a1 = IndexLabel(1, :a)
+    b1 = IndexLabel(1, :b)
+    c1 = IndexLabel(1, :c)
+    a2 = IndexLabel(2, :a)
+    b3 = IndexLabel(3, :b)
+    
+    tl1 = TensorLabel(1, [a1, b1, c1])
+    tl2 = TensorLabel(2, [a2])
+    tl3 = TensorLabel(3, [b3])
+    
+    add_tensor!(tn, tl1)
+    add_tensor!(tn, tl2)
+    add_tensor!(tn, tl3)
+    
+    @test Set(find_indices(tn) do idx idx.group == 2 || idx.group == 3 end) == Set([a2, b3])
+    
+    @test Set(find_indices_by_group(tn, 1)) == Set([a1, b1, c1])
+    @test Set(find_indices_by_group(tn, 2)) == Set([a2])
+    @test Set(find_indices_by_group(tn, 3)) == Set([b3])
+    
+    @test Set(find_indices_by_port(tn, :a)) == Set([a1, a2])
+    @test Set(find_indices_by_port(tn, :b)) == Set([b1, b3])
+    @test Set(find_indices_by_port(tn, :c)) == Set([c1])
+    
+    @test find_index(tn, 1, :a) == IndexLabel(1, :a) == a1
+    @test_throws ErrorException find_index(tn, 2, :b)
 end
---
+++ test/visualizertest.jl
@@ -0,0 +1,38 @@
+using FibTN.Indices
+using FibTN.TensorNetworks
+using FibTN.Visualizer
+using FibTN.FibTensorTypes
+
+using GLMakie
+
+@testset "visualize TensorNetwork" begin
+    # build network
+    tn = TensorNetwork()
+    add_tensor!(tn, TensorLabel(1, index_labels(Tail, 1)))
+    add_tensor!(tn, TensorLabel(2, index_labels(Tail, 2)))
+    add_tensor!(tn, TensorLabel(3, index_labels(Tail, 3)))
+    add_tensor!(tn, TensorLabel(4, index_labels(LoopAmplitude, 4)))
+    add_contraction!(tn, IndexPair(IndexLabel(1, :b), IndexLabel(4, :a)))
+    add_contraction!(tn, IndexPair(IndexLabel(4, :b), IndexLabel(2, :a)))
+    add_contraction!(tn, IndexPair(IndexLabel(2, :b), IndexLabel(3, :a)))
+    add_contraction!(tn, IndexPair(IndexLabel(3, :b), IndexLabel(1, :a)))
+    
+    # build displayspec
+    positions = [(0, √3), (1, 0), (-1, 0), (1/2, (√3)/2)]
+    colors = [:black, :black, :black, :blue]
+    markers = [:circle, :circle, :circle, :circle]
+    tnds = TensorNetworkDisplaySpec(1:4, positions, colors, markers)
+    
+    # create figure and visualize
+    f = Figure()
+    ax = Axis(f[1, 1])
+    hidedecorations!(ax)
+    hidespines!(ax)
+    visualize(tn, tnds, ax)
+end
+
+@testset "visualize QubitLattice" begin
+    # build qubit lattice
+    ql = QubitLattice()
+    add_index!(ql, IndexLabel(1, :q), [1, 3, 4])
+end
