d663ceafd835a5865bffe2c40f4e7edffe19efe1
nmaserang <nmaserang@berkeley.edu>
2026-01-27 02:45:37 -0800

    changed index manipulation function names and added tail and crossing data

--- src/fibtensortypes.jl
+++ src/fibtensortypes.jl
@@ -47,9 +47,9 @@ index_ports(::Type{Reflector}) = (:a, :b)
 
 index_ports(::Type{LoopAmplitude}) = (:a, :b)
 
-index_ports(::Type{Vertex}) = (:a, :b, :c, :q)
+index_ports(::Type{Vertex}) = (:a, :b, :c, :p)
 
-index_ports(::Type{Tail}) = (:a, :b, :q)
+index_ports(::Type{Tail}) = (:a, :b, :p)
 
 index_ports(::Type{Crossing}) = (:a, :b, :c, :d)
 
@@ -95,30 +95,74 @@ function generate_tensor_data(::Type{LoopAmplitude})
 end
 
 function generate_tensor_data(::Type{Vertex})
-    GSTriangle_data = zeros(Float64, 5, 5, 5, 5)
+    # break out indices
+    function _abc2ijkλμνp(a::Int, b::Int, c::Int)
+        μ, i, ν = split_index(a)
+        ν2, j, λ = split_index(b)
+        λ2, k, μ = split_index(c)
+        # eliminate cases which go to 0 due to inconsistency
+        if μ != μ2 || ν != ν2 || λ != λ2 throw(ArgumentError("incompatible indices")) end
+        i, j, k, λ, μ, ν, combine_indices(i, j, k)
+    end
+
+    # generate data
+    arr = zeros(Float64, 5, 5, 5, 5)
     for a in 1:5
         for b in 1:5
             for c in 1:5
                 local i, j, k, λ, μ, ν, p
-                try i, j, k, λ, μ, ν, p = abc2ijkλμνp(a, b, c) catch; continue end
+                try i, j, k, λ, μ, ν, p = _abc2ijkλμνp(a, b, c) catch; continue end
                 i, j, k, λ, μ, ν = anyon.([i, j, k, λ, μ, ν])
-                GSTriangle_data[a, b, c, p] = Gsymbol(i, j, λ, μ, k, ν) * √√(qdim(i)*qdim(j)*qdim(k))
+                arr[a, b, c, p] = Gsymbol(i, j, λ, μ, k, ν) * √√(qdim(i)*qdim(j)*qdim(k))
             end
         end
     end
-    GSTriangle_data
+    arr
 end
 
 function generate_tensor_data(::Type{Tail})
-    [1 0 0 0 0;
-     0 0 0 0 0;
-     0 0 1 0 0;
-     0 0 0 0 0;
-     0 0 0 0 0]
+    # break out indices
+    function _ab2xyμνp(a::Int, b::Int)
+        μ, x, ν = p2ijk(a)
+        ν2, y, μ2 = p2ijk(b)
+        # eliminate cases which go to 0 due to inconsistency
+        if μ != μ2 || ν != ν2 throw(ArgumentError("incompatible indices")) end
+        x, y, μ, ν, combine_indices(x, 1, y)
+    end
+
+    # generate data
+    arr = zeros(Float64, 5, 5, 5)
+    for a in 1:5
+        for b in 1:5
+            try x, y, μ, ν, p = _ab2xyμνp(a, b) catch; continue end
+            arr[a, b, p] = x == y ? 1 : 0
+        end
+    end
+    arr
 end
 
 function generate_tensor_data(::Type{Crossing})
-    # TODO
+    # break out indices
+    function _abcd2ksλμνξ(a::Int, b::Int, c::Int, d::Int)
+        ξ, k, λ = split_index(a)
+        λ2, s, μ = split_index(b)
+        μ2, k2, ν = split_index(c)
+        ν2, s2, ξ2 = split_index(d)
+        if ξ != ξ2 || λ != λ2 || μ != μ2 || ν != ν2 || k != k2 || s != s2 throw(ArgumentError("incompatible indices")) end
+        k, s, λ, μ, ν, ξ
+    end
+    arr = zeros(Float64, 5, 5, 5, 5)
+    for a in 1:5
+        for b in 1:5
+            for c in 1:5
+                for d in 1:5
+                    try k, s, λ, μ, ν, ξ = _abcd2ksλμνξ(a, b, c, d) catch; continue end
+                    arr[a, b, c, d] = Gsymbol(μ, λ, ξ, ν, s, k)
+                end
+            end
+        end
+    end
+    arr
 end
 
 function generate_tensor_data(::Type{Fusion})
--- test/indextripletstest.jl
+++ test/indextripletstest.jl
@@ -1,24 +1,24 @@
 using FibTN.IndexTriplets
 
-@testset "ijk2p basics" begin 
-    @test ijk2p(0, 0, 0) == 1
-    @test ijk2p(0, 1, 1) == 2
-    @test ijk2p(1, 0, 1) == 3
-    @test ijk2p(1, 1, 0) == 4
-    @test ijk2p(1, 1, 1) == 5
+@testset "combine_indices basics" begin 
+    @test combine_indices(0, 0, 0) == 1
+    @test combine_indices(0, 1, 1) == 2
+    @test combine_indices(1, 0, 1) == 3
+    @test combine_indices(1, 1, 0) == 4
+    @test combine_indices(1, 1, 1) == 5
 end
 
-@testset "p2ijk basics" begin
-    @test p2ijk(1) == (0, 0, 0)
-    @test p2ijk(2) == (0, 1, 1)
-    @test p2ijk(3) == (1, 0, 1)
-    @test p2ijk(4) == (1, 1, 0)
-    @test p2ijk(5) == (1, 1, 1)
+@testset "split_index basics" begin
+    @test split_index(1) == (0, 0, 0)
+    @test split_index(2) == (0, 1, 1)
+    @test split_index(3) == (1, 0, 1)
+    @test split_index(4) == (1, 1, 0)
+    @test split_index(5) == (1, 1, 1)
 end
 
 @testset "p2ijk2p roundtrip" begin
-    for p in 1:5
-        i, j, k = p2ijk(p)
-        @test ijk2p(i, j, k) == p
+    for a in 1:5
+        i, j, k = split_index(a)
+        @test combine_indices(i, j, k) == a
     end
 end
